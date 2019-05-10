005f9831e2a4b51590746861809e71072cdc6bab
carol975 <carolf@alumni.ubc.ca>
2019-05-09 21:46:44 -0700

    add successor for in order search

--- study/Trees/Node.java
+++ study/Trees/Node.java
@@ -5,9 +5,11 @@ public class Node {
 	int val;
 	Node left;
 	Node right;
+	Node parent;
 	public Node(int val){
 		this.val = val;
 		this.left = null;
 		this.right = null;
+		this.parent = null;
 	}
 }
